0ea5ce9b859b5f4f334902e3dc198454a775c5a6
franco <fganga@ymail.com>
2019-10-25 15:48:41 -0300

    Agregando fixtures para testear vista de persona

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\DirectorCarrera;
 use App\Entity\Persona;
 use App\Entity\Instituto;
+use App\Entity\ProyectoExtension;
 use App\Entity\ProyectoInvestigacion;
 use App\Entity\Carrera;
 use App\Entity\Materia;
@@ -54,10 +56,62 @@ class AppFixtures extends Fixture
             $manager->persist($proyecto);
         }
 
-        
-        
+        /**
+         * Crear persona con muchas actividades
+         */
+        $persona = new Persona();
+        $persona->setNombre("Test");
+        $persona->setApellido("persona");
 
+        for ($i = 0; $i < 2; $i++) {
+            $proyectoExtension = new ProyectoExtension();
+            $proyectoExtension->setNombre($faker->word);
+            $miembroProyecto = new MiembroProyecto();
+            $now = date('Y-m-d');
+            $dateTime = new DateTime($now);
+            $miembroProyecto->setInicio($dateTime);
+            $miembroProyecto->setPersona($persona);
+            $miembroProyecto->setProyecto($proyectoExtension);
+            $manager->persist($proyectoExtension);
+            $manager->persist($miembroProyecto);
+        }
+
+        $coordinadorMateria = new CoordinadorMateria();
+
+        $now = $this->dateNow();
+        $coordinadorMateria->setInicio($now);
+        $coordinadorMateria->setPersona($persona);
+        $materia = new Materia();
+        $materia->setNombre($faker->word);
+        $coordinadorMateria->setMateria($materia);
+        $manager->persist($coordinadorMateria);
+        $manager->persist($materia);
+
+        $directorCarrera = new DirectorCarrera();
+        $directorCarrera->setPersona($persona);
+        $directorCarrera->setInicio($now);
+        $carrera = new Carrera();
+        $carrera->setNombre($faker->word);
+
+        $directorCarrera->setCarrera($carrera);
+        
+        $manager->persist($directorCarrera);
+        $manager->persist($carrera);
+
+        $manager->persist($persona);
 
         $manager->flush();
+    }
+
+    /**
+     * Returns current time as DateTime
+     *
+     * @return DateTime
+     */
+    public static function dateNow()
+    {
+        $now = date('Y-m-d');
+
+        return new DateTime($now);
     }
 }
