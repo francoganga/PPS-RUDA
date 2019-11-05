0f77701bc6100ab4349e5eb116de30eeb563c3af
franco <fganga@ymail.com>
2019-11-04 23:39:51 -0300

    Cambiando label del boton de acuerdo al estado:
    Si el área de detalles de la actividad está abierta sera "Ocultar", de
    lo contrario sera "Mostrar"

--- public/js/VistaPersona.js
+++ public/js/VistaPersona.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
    $('.slide').off().on('click', function(){
        var target = $(this).attr('target');
        var selector = "#".concat(target);
+       if ($(this).text() === "Mostrar") {
+           $(this).html("Ocultar")
+       } else if ($(this).text() === "Ocultar") {
+           $(this).html("Mostrar")
+       }
 
        $(selector).slideToggle('fast');
-   }); 
+   });
 });
