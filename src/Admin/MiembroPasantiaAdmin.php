9bdc4d8233c2a92a85e7e95f92b685111d79dc1e
franco <fganga@ymail.com>
2019-11-12 17:02:00 -0300

    Agregado admins de pasantias y correcciones.

--- src/Admin/MiembroPasantiaAdmin.php
+++ src/Admin/MiembroPasantiaAdmin.php
@@ -8,34 +8,17 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Route\RouteCollection;
-use Sonata\AdminBundle\Show\ShowMapper;
 use Sonata\AdminBundle\Form\Type\ModelListType;
-use Sonata\AdminBundle\Form\Type\ModelType;
-use Sonata\Form\Validator\ErrorElement;
+use Sonata\AdminBundle\Show\ShowMapper;
 
-final class MiembroProyectoAdmin extends AbstractAdmin
+final class MiembroPasantiaAdmin extends AbstractAdmin
 {
-    /**
-     * Eliminar rutas base
-     * Mantener solo las generadas
-     * a partir del padre
-     *
-     * @return void
-     */
-    protected function configureRoutes(RouteCollection $collection)
-    {
-        if ($this->isChild()) {
-            return;
-        }
-
-        $collection->clear();
-    }
-
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
     {
         $datagridMapper
+            ->add('inicio')
+            ->add('fin')
             ;
     }
 
@@ -44,6 +27,8 @@ final class MiembroProyectoAdmin extends AbstractAdmin
         $listMapper
             ->add('persona.nombre', null, ['label' => 'Nombre'])
             ->add('persona.apellido', null, ['label' => 'Apellido'])
+            ->add('inicio')
+            ->add('fin')
             ->add('_action', null, [
                 'actions' => [
                     'show' => [],
@@ -55,22 +40,8 @@ final class MiembroProyectoAdmin extends AbstractAdmin
 
     protected function configureFormFields(FormMapper $formMapper): void
     {
-
-        $em = $this->getModelManager()->getEntityManager('App\Entity\RolProyecto');
-
-        $query = $em->createQuery(
-            "SELECT r FROM App\Entity\RolProyecto r JOIN r.proyectos p WHERE p.id=:pid"
-        );
-        $proyecto = $this->getSubject()->getProyecto();
-
-        $query->setParameter("pid", $proyecto->getId());
-
         $formMapper
             ->add('persona', ModelListType::class)
-            ->add('rol', ModelType::class, [
-                "query" => $query,
-                "btn_add" => false
-            ])
             ->add('inicio')
             ->add('fin')
             ;
@@ -81,19 +52,8 @@ final class MiembroProyectoAdmin extends AbstractAdmin
         $showMapper
             ->add('persona.nombre', null, ['label' => 'Nombre'])
             ->add('persona.apellido', null, ['label' => 'Apellido'])
-            ;
-    }
-
-    public function validate(ErrorElement $errorElement, $object)
-    {
-        $errorElement
-            ->with('rol')
-                ->assertNotNull()
-                ->assertNotBlank()
-                ->end()
-            ->with('persona')
-               ->assertNotBlank()
-            ->end()
+            ->add('inicio')
+            ->add('fin')
             ;
     }
 }
