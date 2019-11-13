3bcf55833405036a3160d4c6d50429ee31d6acc6
franco <fganga@ymail.com>
2019-11-13 11:27:16 -0300

    Agregado: entidades de vinculador.

--- src/Entity/Actividad.php
+++ src/Entity/Actividad.php
@@ -26,7 +26,8 @@ use \Datetime;
  * "miembro_pps" = "MiembroPPS", "miembro_actividadDivulgacion" = "MiembroActividadDivulgacion",
  * "miembro_cursoExtension" = "MiembroCursoExtension", "rector" = "Rector",
  * "reponsable_area" = "ResponsableArea", "miembro_pasantia" = "MiembroPasantia",
- * "miembro_voluntariado" = "MiembroVoluntariado", "miembro_programa" = "MiembroPrograma"})
+ * "miembro_voluntariado" = "MiembroVoluntariado", "miembro_programa" = "MiembroPrograma",
+ * "vinculador" = "Vinculador"})
  * @Gedmo\SoftDeleteable(fieldName="deletedAt", timeAware=false, hardDelete=true)
  */
 abstract class Actividad
---
+++ src/Entity/Vinculador.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\VinculadorRepository")
+ */
+class Vinculador extends Actividad
+{
+
+    public function getDatos()
+    {
+    }
+}
