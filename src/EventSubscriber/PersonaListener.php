0df79afbd43b0431fa087efd04d621775d348d12
franco <fganga@ymail.com>
2019-11-21 16:25:09 -0300

    Modificando event listener de Persona:
    - De esta menera se adquieren los datos de mapuche y se setean en
    propiedades individuales en la entidad de persona.

--- src/EventSubscriber/PersonaListener.php
+++ src/EventSubscriber/PersonaListener.php
@@ -20,33 +20,23 @@ class PersonaListener
 
     /**
      * Http request a API, luego que se carga el objeto persona.
+     * Luego se llama a cada setter de las propiedades en persona
+     * iterando sobre las keys de la respuesta de mapuche.
+     * por cada key ejecutar $persona->set{key capitalizada}(parametro=respuesta de mapuche[key])
      *
      * @return void
      */
     public function postLoad(Persona $persona, LifecycleEventArgs $args)
     {
-        $response = $this->mapuche->request('GET', 'agentes/'.$persona->getLegajo());
+        $response = $this->mapuche->request('GET', 'agentes/'.$persona->getIdMapuche());
         $results = $response->getBody()->getContents();
-        $normResults = json_decode($results, true);
+        $normalizedResults = json_decode($results, true);
 
-        $deletedValues = [
-            'legajo',
-            'agente',
-            'desc_apmat',
-            'documento',
-            'cuil',
-            'estado',
-            'descripcion_estado',
-            'datos_combo',
-            'fecha_jubilacion',
-            'fecha_ingreso'
-        ];
+        $fields = array_keys($normalizedResults);
 
-        foreach ($deletedValues as $value) {
-            unset($normResults[$value]);
+        for ($i = 1; $i < sizeof($fields); $i++) { /* Se recorre todas las key de la respuesta de mapuche*/
+            $capitalizedValue = ucfirst($fields[$i]); /* Capitalizacion para setters en entidad asi el metodo queda de forma camelcase */
+            call_user_func([$persona, 'set'.$capitalizedValue], $normalizedResults[$fields[$i]]); /* Se llama a cada setter de Persona*/
         }
-
-        $persona->setDatosMapuche($normResults);
-
     }
 }
